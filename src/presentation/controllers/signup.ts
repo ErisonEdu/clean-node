739abe43015ab5c4764ec292180481829242d78e
Erison Eduard <erison.pimentao@gmail.com>
2022-05-19 00:45:01 -0300

    feat: ensre SignUpController returns 400 if an invalid email is provided

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,16 +1,34 @@
 import { HttpRequest, HttpResponse } from '../protocols/http'
 import { MissingParamError } from '../errors/missing-param-error'
+import { InvalidParamError } from '../errors/invalid-param-error'
 import { badRequest } from '../helpers/http-helper'
 import { Controller } from '../protocols/controller'
+import { EmailValidator } from '../protocols/email-validator'
 
 export class SignUpController implements Controller {
+  private readonly emailvalidator: EmailValidator
+  constructor (emailValidator: EmailValidator) {
+    this.emailvalidator = emailValidator
+  }
+
   handle (httpRequest: HttpRequest): HttpResponse {
-    const requiredFields = ['name', 'email', 'password', 'passwordConfirmation']
-    for (const field of requiredFields) {
-      if (!httpRequest.body[field]) {
-        return badRequest(new MissingParamError(field))
+    try {
+      const requiredFields = ['name', 'email', 'password', 'passwordConfirmation']
+      for (const field of requiredFields) {
+        if (!httpRequest.body[field]) {
+          return badRequest(new MissingParamError(field))
+        }
       }
+      const isValid = this.emailvalidator.isValid(httpRequest.body.email)
+      if (!isValid) {
+        return badRequest(new InvalidParamError('email'))
+      }
+      return {
+        statusCode: 201,
+        body: 'created'
+      }
+    } catch (error) {
+      throw new Error(error)
     }
-    throw new Error('All fields are required')
   }
 }
